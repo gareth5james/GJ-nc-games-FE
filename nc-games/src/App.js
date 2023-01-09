e954395a2d73f76fbfac01a6eda7cfe2029612da
gareth5james <gareth5james@yahoo.co.uk>
2023-01-09 12:33:50 +0000

    add Route and Routes to app.js

--- nc-games/src/App.js
+++ nc-games/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Nav from "./Components/Nav";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
+      <Routes></Routes>
     </div>
   );
 }
