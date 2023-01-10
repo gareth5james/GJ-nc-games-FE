45807f766d661c842d296b9e433e308c5d32f573
gareth5james <gareth5james@yahoo.co.uk>
2023-01-10 12:12:23 +0000

    restrict to 1 vote

--- nc-games/src/Components/ReviewVotes.jsx
+++ nc-games/src/Components/ReviewVotes.jsx
@@ -4,21 +4,31 @@ import {patchReviewVotes} from "../api"
 function ReviewVotes({review}) {
     const [votes, setVotes] = useState(0)
     const [isVoteError, setIsVoteError] = useState(false)
+    const [hasVoted, setHasVoted] = useState(false)
+    const [hasVotedError, setHasVotedError] = useState(false)
 
     useEffect(() => {
         setVotes(review.votes)
     },[review.votes])
 
     function incVotes(inc) {
+        setHasVotedError(false)
+        if(!hasVoted) {
         setIsVoteError(false);
+        setHasVoted(true)
         setVotes((currentVotes) => currentVotes + inc);
         patchReviewVotes(review.review_id, {inc_votes: inc}).catch(e => {
             setVotes(review.votes)
             setIsVoteError(true)
+            setHasVoted(false)
         });
+    } else {
+        setHasVotedError(true)
+    }
     }
 
     return <div className="review__Votes">
+    {hasVotedError ? <p className="errorMessage">You can only vote on each review once</p> : null}
     {isVoteError ? <p className="errorMessage">Error updating votes</p> : null}
     <p>Votes: {votes}</p>
     <button type="button" onClick={() => {incVotes(1)}}>👍</button>
