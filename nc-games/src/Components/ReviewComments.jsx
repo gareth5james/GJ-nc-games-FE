b047f7a1e659da1f95003b9851abfaa4b28fd001
gareth5james <gareth5james@yahoo.co.uk>
2023-01-10 10:49:09 +0000

    start vote buttons

--- nc-games/src/Components/ReviewComments.jsx
+++ nc-games/src/Components/ReviewComments.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react"
-import { getComments } from "../api"
+import { getComments, patchReviewVotes } from "../api"
 
 function ReviewComments(reviewId) {
     const [comments, setComments] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [votes, setVotes] = useState(0)
 
     useEffect(() => {
         setIsLoading(true)
@@ -13,13 +14,21 @@ function ReviewComments(reviewId) {
         })
     }, [reviewId])
 
+    useEffect(() => {
+        setVotes(review.votes)
+    })
+
+    function incVotes(inc) {
+        patchReviewVotes(reviewId, {inc_votes: inc})
+    }
+
     return <ul className="comments">
         {isLoading ? <p>Comments Loading</p> : comments.map(comment => {
             return <li key={comment.comment_id}>
                 <p>{comment.body}</p>
                 <p>By: {comment.author}</p>
                 <p>Votes: {comment.votes}</p>
-                <button>👍</button><button>👎</button>
+                <button onClick={() => {incVotes(1)}}>👍</button><button onClick={() => {incVotes(-1)}}>👎</button>
             </li>
         })}
     </ul>
