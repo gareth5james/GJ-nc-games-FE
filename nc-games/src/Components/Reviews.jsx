042c00469f3a38e5c2d688ceaa4e7b347cb1fae5
gareth5james <gareth5james@yahoo.co.uk>
2023-01-12 10:46:36 +0000

    update sorting

--- nc-games/src/Components/Reviews.jsx
+++ nc-games/src/Components/Reviews.jsx
@@ -23,11 +23,13 @@ function Reviews({sortBy, setSortBy, orderBy, setOrderBy}) {
                         <div className="reviewList__Header">
                             <p>Title: {review.title}</p>
                             <p>Category: {review.category}</p>
-                            <p>Designer: {review.designer}</p>
+                            <p>Designer: {review.designer} on: {new Date(review.created_at).toDateString()}</p>
                         </div>
                         <div className="reviewList__Body">
                             <p>{review.review_body}</p>
                             <p>By: {review.owner}</p>
+                            <p>Votes: {review.votes}</p>
+                            <p>Comments: {review.comment_count}</p>
                         </div>
                         <Link to={`/reviews/${review.review_id}`}>See More</Link>
                     </li>
--- nc-games/src/Components/SortBy.jsx
+++ nc-games/src/Components/SortBy.jsx
@@ -3,12 +3,12 @@ function SortBy ({sortBy, setSortBy, orderBy, setOrderBy}) {
                 <select id="sort_by" value={sortBy} onChange={(event => {
                     setSortBy(event.target.value)
                     })}>
-                    <option key="review_id" value="review_id">Review ID</option>
                     <option key="title" value="title">Title</option>
                     <option key="designer" value="designer">Designer</option>
                     <option key="owner" value="owner">Owner</option>
                     <option key="created_at" value="created_at">Date</option>
                     <option key="votes" value="votes">Votes</option>
+                    <option key="comment_count" value="comment_count">Number of Comments</option>
                 </select>
                 <select id="order_by" value={orderBy} onChange={(event) => {
                     setOrderBy(event.target.value)
